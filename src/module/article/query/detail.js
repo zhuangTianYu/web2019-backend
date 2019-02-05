46f385ab97a2301a7439d344b4ab3a54bf409431
Zhuang TianYu <zhuangtianyu@weimingedu.com>
2019-02-05 13:27:22 +0800

    开发 list, detail, write 接口

--- src/module/article/query/detail.js
+++ src/module/article/query/detail.js
@@ -1,5 +1,34 @@
-const detail = async ctx => {
-  ctx.body = { title: 'title-0', content: 'content' }
+const fs = require('fs')
+const query = require('../../../util/query')
+
+const detail = async (ctx, next) => {
+  const id = ctx.params.id
+  const createTime = 'DATE_FORMAT(createTime, "%Y-%m-%d") as createTime'
+  const select = `select title, author, ${createTime} from article`
+  const where = `where id = ${id}`
+  const selectSql = `${select} ${where}`
+
+  const body = {}
+
+  const service = new Promise((resolve, reject) => {
+    query(selectSql, (result) => {
+      const { title, author, createTime } = result[0]
+      Object.assign(body, { title, author, createTime })
+      // 读取 json 文件
+      fs.readFile(`file/article-${id}.json`, (error, data) => {
+        if (error !== null) { throw error }
+        resolve(data)
+      })
+    })
+  })
+
+  await service.then((data) => {
+    const { content, comment } = JSON.parse(data)
+    Object.assign(body, { content, comment })
+  })
+
+  // 返回数据
+  ctx.body = body
 }
 
 module.exports = detail
--- src/module/article/query/list.js
+++ src/module/article/query/list.js
@@ -1,7 +1,16 @@
 const query = require('../../../util/query')
 
 const list = async (ctx, next) => {
-  await query('select * from article', (result) => {
+  const type = ctx.params.type
+  const createTime = 'DATE_FORMAT(createTime, "%Y-%m-%d") as createTime'
+  const select = `select id, title, author, comment, ${createTime} from article`
+  const where =
+    type === '0'
+      ? ''
+      : `where type = ${type}`
+  const order = 'order by createTime'
+  const selectSql = `${select} ${where} ${order}`
+  await query(selectSql, (result) => {
     ctx.body = result
     next()
   })
